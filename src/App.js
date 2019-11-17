2c709c1f9a0fb2fb2c257cb28d2fd02cd2ff90b6
Matt <51001542+itsTadae@users.noreply.github.com>
2019-11-17 16:46:08 -0500

    Changed Learn React Here (#2)
    * Changed Learn React Here
    
    * Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Here
+          Learn React Now
         </a>
       </header>
     </div>
